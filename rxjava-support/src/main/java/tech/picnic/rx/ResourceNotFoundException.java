767716fb54888e5b6532aee8c3347b227de4be22
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2018-09-25 11:07:03 +0200

    Resolve compilation warnings

--- rxjava-support/src/main/java/tech/picnic/rx/ResourceNotFoundException.java
+++ rxjava-support/src/main/java/tech/picnic/rx/ResourceNotFoundException.java
@@ -3,5 +3,9 @@ package tech.picnic.rx;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/** Indicates that a requested resource was not found. */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {}
+public class ResourceNotFoundException extends RuntimeException {
+  /** The {@link java.io.Serializable serialization} ID. */
+  private static final long serialVersionUID = 1L;
+}
